a6efd3ae341fdef4e78636504d84518108522424
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-06 09:42:28 -0500

    feat: ensure AxiosHttpClient calls axios with correct values

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   moduleNameMapper: {
     '@domain/(.*)$': '<rootDir>/src/domain/$1',
     '@data/(.*)$': '<rootDir>/src/data/$1',
+    '@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
     '@tests/(.*)$': '<rootDir>/tests/$1'
   },
   transform: {
