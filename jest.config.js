2b3a9e56a670fe3dbe9d149b9187d0f03687fc12
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-05 19:59:20 -0500

    refactor: add @domain pathMapper

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.(ts,tsx)'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '@domain/(.*)$': '<rootDir>/src/domain/$1'
+  },
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
