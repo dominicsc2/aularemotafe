deba563e3fa13c139e278e1577a0a2420a93a791
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-13 16:35:59 -0500

    feat: add axios http client

---
+++ src/clean/infrastructure/http/axios-http-client.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+import { HttpClient, HttpRequestParams, HttpResponse } from '@clean/data/protocols/http';
+
+export class AxiosHttpClient<BodyType, ResponseType> implements HttpClient<BodyType, ResponseType> {
+  async request(data: HttpRequestParams<BodyType>): Promise<HttpResponse<ResponseType>> {
+    let axiosResponse: AxiosResponse<ResponseType, BodyType>;
+    try {
+      axiosResponse = await axios.request({
+        url: data.url,
+        method: data.method,
+        data: data.body,
+        headers: data.headers
+      });
+    } catch (error: any) {
+      axiosResponse = error.response;
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    };
+  }
+}
