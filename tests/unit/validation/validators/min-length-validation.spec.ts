8943b3db07a8ef1c6fd0dabd0ed830e2ac16cde4
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-14 18:41:55 -0500

    test: ensure MinLengthValidation returns an error if field length is less than specify value

---
+++ tests/unit/validation/validators/min-length-validation.spec.ts
@@ -0,0 +1,16 @@
+import { MinLengthError } from '@clean/validation/errors'
+import { FieldValidation } from '@clean/validation/protocols'
+import { MinLengthValidation } from '@clean/validation/validators'
+import faker from '@faker-js/faker'
+
+const makeSut = (field: string, minLegth: number): FieldValidation => new MinLengthValidation(field, minLegth)
+
+describe('MinLengthValidation', () => {
+  test('Should return an error if field length is less than specify value', () => {
+    const fieldName = faker.database.column()
+    const minLength = 5
+    const sut = makeSut(fieldName, minLength)
+    const error = sut.validate({ [fieldName]: faker.random.alphaNumeric(3) })
+    expect(error).toEqual(new MinLengthError(fieldName, minLength))
+  })
+})
