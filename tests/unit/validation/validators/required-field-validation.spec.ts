4429148fca9f1f73bf242ad166b4f512b79c6b2f
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-14 18:42:45 -0500

    refactor: update makeSut return type to return FieldValidation interface

--- tests/unit/validation/validators/required-field-validation.spec.ts
+++ tests/unit/validation/validators/required-field-validation.spec.ts
@@ -1,9 +1,10 @@
-import faker from "@faker-js/faker"
-import { RequiredFieldValidation } from "@clean/validation/validators"
 import { RequiredFieldError } from "@clean/validation/errors"
+import { FieldValidation } from "@clean/validation/protocols"
+import { RequiredFieldValidation } from "@clean/validation/validators"
+import faker from "@faker-js/faker"
 
 
-const makeSut = (field: string): RequiredFieldValidation => new RequiredFieldValidation(field)
+const makeSut = (field: string): FieldValidation => new RequiredFieldValidation(field)
 
 describe('RequiredFieldValidation', () => {
   test('Should return an error if field is empty', () => {
