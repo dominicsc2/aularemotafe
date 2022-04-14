887e681cf0f07bcf33127d3704737b248a555185
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-14 18:18:28 -0500

    test: ensure ValidationComposite returns falsy if validations succeeds

--- tests/unit/validation/validators/validation-composite.spec.ts
+++ tests/unit/validation/validators/validation-composite.spec.ts
@@ -1,30 +1,37 @@
-import { ValidationComposite } from '@clean/validation/validators'
 import faker from '@faker-js/faker'
+import { ValidationComposite } from '@clean/validation/validators'
 import { FieldValidationSpy } from '../mocks/mock-validation-composite'
 
 type SutTypes = {
   fieldValidationsSpy: FieldValidationSpy[]
-  validationComposite: ValidationComposite
+  sut: ValidationComposite
 }
 
 const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [new FieldValidationSpy(fieldName), new FieldValidationSpy(fieldName)]
-  const validationComposite = ValidationComposite.build(fieldValidationsSpy)
+  const sut = ValidationComposite.build(fieldValidationsSpy)
 
   return {
     fieldValidationsSpy,
-    validationComposite
+    sut
   }
 }
 
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
     const fieldName = faker.database.column()
-    const { fieldValidationsSpy, validationComposite } = makeSut(fieldName)
+    const { fieldValidationsSpy, sut } = makeSut(fieldName)
     const errorMessage = faker.random.words()
     fieldValidationsSpy[0].error = new Error(errorMessage)
     fieldValidationsSpy[1].error = new Error(faker.random.words())
-    const error = validationComposite.validate(fieldName, { [fieldName]: faker.random.word() })
+    const error = sut.validate(fieldName, { [fieldName]: faker.random.word() })
     expect(error).toBe(errorMessage)
+  })
+
+  test('Should return falsy if validations succeeds', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, { [fieldName]: faker.random.words() })
+    expect(error).toBeFalsy()
   })
 })
