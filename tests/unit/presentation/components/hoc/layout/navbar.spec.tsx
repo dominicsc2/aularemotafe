118983efa718e3c666ba6881c8a80a98a29d6a2c
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-13 22:22:21 -0500

    test: ensure Navbar goes to signup page on register button clicked

---
+++ tests/unit/presentation/components/hoc/layout/navbar.spec.tsx
@@ -0,0 +1,39 @@
+import { Navbar } from '@clean/presentation/components/hoc'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}))
+
+jest.mock('React', () => ({
+  ...jest.requireActual('React'),
+  useEffect: jest.fn()
+}))
+
+const makeSut = (): void => {
+  render(
+    <Navbar>
+      <div></div>
+    </Navbar>
+  )
+}
+
+describe('Navbar component', () => {
+  test('Should go to signup page on register button clicked', () => {
+    const push = jest.fn()
+    const pathname = jest.fn()
+
+    ;(useRouter as jest.Mock).mockImplementationOnce(() => ({
+      push,
+      pathname
+    }))
+
+    makeSut()
+
+    const button = screen.getByTestId('signup-button')
+    fireEvent.click(button)
+    expect(push).toHaveBeenCalledWith('/accounts/signup')
+  })
+})
