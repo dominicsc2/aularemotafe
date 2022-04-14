7235f74b3f814ec2b034fdc4beb2c6568aa9205b
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-14 18:56:06 -0500

    feat: ensure EmailValidation returns an error if email is invalid

---
+++ src/clean/validation/validators/email-validation.ts
@@ -0,0 +1,12 @@
+import { InvalidEmailError } from "../errors";
+import { FieldValidation } from "../protocols";
+
+export class EmailValidation implements FieldValidation {
+  constructor(readonly field: string) {}
+
+  validate(input: Record<string, any>): Error {
+    const emailRegex =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return (!input[this.field] || emailRegex.test(input[this.field])) ? null : new InvalidEmailError()
+  }
+
+}
--- src/clean/validation/validators/index.ts
+++ src/clean/validation/validators/index.ts
@@ -1,3 +1,4 @@
 export * from './validation-composite'
 export * from './required-field-validation'
 export * from './min-length-validation'
+export * from './email-validation'
