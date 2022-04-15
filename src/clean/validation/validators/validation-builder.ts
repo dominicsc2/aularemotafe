e1ae95b7f290c88722e89f7324d378738591a317
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-14 19:19:02 -0500

    feat: ensure ValidationBuilder returns RequiredFieldValidation

---
+++ src/clean/validation/validators/validation-builder.ts
@@ -0,0 +1,37 @@
+import { FieldValidation } from "../protocols";
+import { CompareFieldsValidation } from "./compare-fields-validation";
+import { EmailValidation } from "./email-validation";
+import { MinLengthValidation } from "./min-length-validation";
+import { RequiredFieldValidation } from "./required-field-validation";
+
+export class ValidationBuilder {
+  private constructor(private readonly fieldName: string, private readonly validations: FieldValidation[]) {}
+
+  static field(fieldName: string): ValidationBuilder {
+    return new ValidationBuilder(fieldName, [])
+  }
+
+  required(): ValidationBuilder {
+    this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  email(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName))
+    return this
+  }
+
+  min(length: number): ValidationBuilder {
+    this.validations.push(new MinLengthValidation(this.fieldName, length))
+    return this
+  }
+
+  sameAs(fieldToCompare: string): ValidationBuilder {
+    this.validations.push(new CompareFieldsValidation(this.fieldName, fieldToCompare))
+    return this
+  }
+
+  build(): FieldValidation[] {
+    return this.validations
+  }
+}
