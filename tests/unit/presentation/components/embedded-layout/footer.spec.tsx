286be4889a9e61947fe9e92029194b1a4de1dc0d
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-16 17:31:17 -0500

    test: ensure Footer renders footer with correct ui

---
+++ tests/unit/presentation/components/embedded-layout/footer.spec.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "@clean/presentation/components/embedded-layout"
+import { render, screen } from "@testing-library/react"
+
+const makeSut = (): void => {
+  render(<Footer />)
+}
+
+describe('Footer component', () => {
+  test('Should render footer with correct ui', () => {
+    makeSut()
+    const footer = screen.getByRole('contentinfo')
+    expect(footer).toBeInTheDocument()
+    const paragraphs = footer.childNodes
+    expect(paragraphs[0]).toHaveTextContent('Trilce')
+    expect(paragraphs[1]).toHaveTextContent('© 2020 Trilce, todos los derechos reservados.')
+  })
+})
