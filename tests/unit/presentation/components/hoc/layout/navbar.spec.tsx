34f5b5c45f7074e5e1b399fa609c848fb908144c
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-16 18:16:56 -0500

    test: ensure Navbar closes offer when close offer button is clicked

--- tests/unit/presentation/components/hoc/layout/navbar.spec.tsx
+++ tests/unit/presentation/components/hoc/layout/navbar.spec.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from '@clean/presentation/components/hoc'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react'
 import { createMockRouter } from '@tests/unit/presentation/helpers'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import { NextRouter } from 'next/router'
@@ -49,6 +49,14 @@ describe('Navbar component', () => {
   test('Should start with header 50px margin top on initial state', () => {
     makeSut()
     expect(screen.getByTestId('header-container').classList.length).toBe(2)
-    expect(screen.getByTestId('header-container').className).toBe("container move")
+    expect(screen.getByTestId('header-container').className).toBe('container move')
+  })
+
+  test('Should close offer when close offer button is clicked', async () => {
+    makeSut()
+    const closeOfferBtn = screen.getByTestId('close-offer')
+    fireEvent.click(closeOfferBtn)
+    expect(screen.queryByTestId('offer-wrap').style.display).toBe('none')
+    expect(screen.getByTestId('header-container').className).toBe('container')
   })
 })
