1188bfd2359e675d152e5b177b72c9b28c0127a5
dominicsc2 <dominicsc2hs@gmail.com>
2022-04-15 20:06:29 -0500

    feat: ensure SignupPage shows a spinner on submit

--- src/clean/presentation/components/common/Spinner/Spinner.tsx
+++ src/clean/presentation/components/common/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import { ISpinner } from '../interfaces/spinner.interfaces';
 import styles from './spinner.module.scss';
 
 const Spinner: React.FC<ISpinner> = (props) => (
-  <div className={props.type === 'spinner-replace' ? styles.spinnerReplace : styles.spinner} />
+  <div data-testid="spinner" className={props.type === 'spinner-replace' ? styles.spinnerReplace : styles.spinner} />
 );
 
 export default Spinner;
